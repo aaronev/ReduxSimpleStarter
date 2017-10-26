baf3f468c17aa699eb4f313824c7180fd892011a
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-10-26 13:49:20 -0700

    complets google map wether api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import  axios from 'axios'
+
 const API_KEY = 'b51b641219666b974b540f2b14ac7871'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
@@ -7,6 +9,8 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
   const request = axios.get(url)
 
+  console.log('Request:', request)
+  
   return{
     type: FETCH_WEATHER,
     payload: request
